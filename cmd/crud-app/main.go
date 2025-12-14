ddbd51bc8ca04fcfce7cf27a50954e84bac93a42
dsooraj <dsooraj1342@gmail.com>
2025-12-15 00:04:05 +0530

    update router to use chi

--- cmd/crud-app/main.go
+++ cmd/crud-app/main.go
@@ -4,14 +4,16 @@ import (
 	"fmt"
 	"net/http"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
 	"github.com/gorilla/mux"
 )
 
 func main() {
 
-	r := mux.NewRouter()
+	routerMux := mux.NewRouter()
 
-	sub := r.PathPrefix("/books").Subrouter()
+	sub := routerMux.PathPrefix("/books").Subrouter()
 
 	sub.HandleFunc("/{name}", func(w http.ResponseWriter, r *http.Request) {
 		vars := mux.Vars(r)
@@ -25,6 +27,18 @@ func main() {
 
 	fs := http.FileServer(http.Dir("static/"))
 	http.Handle("/static/", http.StripPrefix("/static/", fs))
+
+	/* Chi Router */
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+
+	r.Get("/{name}", func(w http.ResponseWriter, r *http.Request) {
+		w.Write([]byte("Hello world!! It is time to dive into Go"))
+	})
 
 	http.ListenAndServe(":8080", r)
 }
--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/dsooraj/crud-app
 
 go 1.25.5
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require (
+	github.com/go-chi/chi/v5 v5.2.3
+	github.com/gorilla/mux v1.8.1
+)
